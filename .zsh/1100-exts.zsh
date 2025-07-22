2086432fd2612d48d6a0dd82ef85bc25bbb44e7c
Aubrey Portwood <code@aubreypwd.com>
2025-07-22 15:18:01 -0600

    Don't track this anymore, I will do it with Open With...

--- .zsh/1100-exts.zsh
+++ .zsh/1100-exts.zsh
@@ -10,19 +10,6 @@ reset-exts () {
 
 	# How to figure out the name of an app: 
 	#    osascript -e 'id of app "Finder"'
-
-	# VSCode: com.microsoft.VSCode
-	duti -s com.microsoft.VSCode .php all
-	duti -s com.microsoft.VSCode .js all
-	duti -s com.microsoft.VSCode .jsx all
-	duti -s com.microsoft.VSCode .html all
-	duti -s com.microsoft.VSCode .css all
-	duti -s com.microsoft.VSCode .scss all
-	duti -s com.microsoft.VSCode .sass all
-	duti -s com.microsoft.VSCode .tsx all
-	duti -s com.microsoft.VSCode .sublime-project all
-	duti -s com.microsoft.VSCode .sublime-workspace all
-	duti -s com.microsoft.VSCode .json all
 	
 	# # Built-in Terminal
 	# duti -s com.apple.Terminal .sh all
