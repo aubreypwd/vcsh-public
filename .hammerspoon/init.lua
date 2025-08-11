38227607b4560edac6ac9b951ffd3955557efc80
Aubrey Portwood <code@aubreypwd.com>
2025-08-11 13:56:06 -0600

    Also make sure HazeOver is disabled when there is no window.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -13,14 +13,22 @@ hs.window.filter.new():subscribe(
 	end
 )
 
--- When a window shows, poke HazeOver so it applies the overlay.
+-- When a window shows, poke HazeOver so it re-applies the overlay (sometimes it doesn't).
 hs.window.filter.new():subscribe(
 	{ "windowFocused", "windowVisible", "windowUnminimized", "windowCreated", "windowDestroyed" },
 	function()
 
 		local hazeOverIntensity = 88;
 
-		hs.osascript.applescript(string.format([[tell application "HazeOver" to set intensity to %d]]), hazeOverIntensity - 1 )
-		hs.osascript.applescript(string.format([[tell application "HazeOver" to set intensity to %d]]), hazeOverIntensity )
+		hs.osascript.applescript( string.format( [[tell application "HazeOver" to set intensity to %d]], hazeOverIntensity - 1 ) )
+		hs.osascript.applescript( string.format( [[tell application "HazeOver" to set intensity to %d]], hazeOverIntensity ) )
+	end
+)
+
+-- When there is no window for an app, disable the HazeOver overlay (beacuse it sticks around normally).
+hs.window.filter.new():subscribe(
+	"hasNoWindows",
+	function()
+		hs.osascript.applescript( string.format( [[tell application "HazeOver" to set intensity to %d]], 0 ) )
 	end
 )
