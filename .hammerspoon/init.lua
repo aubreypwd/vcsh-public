31faa152a0ed5dc1569263edac52059241d0deb6
Aubrey Portwood <code@aubreypwd.com>
2025-08-11 13:23:22 -0600

    Make sure HazeOver gets poked when a window shows up.
    This is because sometimes it doesn't activate.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,26 @@
+
+-- ==============================
+-- Windows
+-- ==============================
+
+hs.window.animationDuration = 0 -- Never animate things.
+
+-- Center all newly created windows.
 hs.window.filter.new():subscribe(
 	"windowCreated",
 	function(win)
 		win:centerOnScreen(nil, false, 0)
+	end
+)
+
+-- When a window shows, poke HazeOver so it applies the overlay.
+hs.window.filter.new():subscribe(
+	"windowFocused",
+	function()
+
+		local hazeOverIntensity = 88;
+
+		hs.osascript.applescript(string.format([[tell application "HazeOver" to set intensity to %d]]), hazeOverIntensity - 1 )
+		hs.osascript.applescript(string.format([[tell application "HazeOver" to set intensity to %d]]), hazeOverIntensity )
 	end
 )
