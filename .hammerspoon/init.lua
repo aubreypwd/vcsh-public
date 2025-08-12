a7d29730874e4befcc4aeab526f25f9a4dbf148a
Aubrey Portwood <code@aubreypwd.com>
2025-08-12 11:34:32 -0600

    Use a function for my version of :centerOnScreen() which takes into account the menubar.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,34 +1,63 @@
+-- ==============================
+-- Functions
+-- ==============================
+
+functions = {
+
+	-- My version of :centerOnScreen that takes into account the menubar (like rectangle).
+	centerOnScreen = function( win )
+
+		-- Apps to exclude from doing this...
+		local excludeApps = {
+			["CleanShot X"] = true,
+			["iBar Pro"] = true,
+		};
+
+		if excludeApps[win:application():name()] then
+			return; -- The window should not be fucked with.
+		end
+
+		win:centerOnScreen( nil, false, 0 ); -- Always center the window on screen, totally fine no matter what.
+
+		if math.abs( win:frame().y + win:frame().h ) == math.abs( win:screen():frame().y + win:screen():frame().h ) then
+			return; -- Don't adjust it for the top menubar if the window is already at the bottom of the screen (will nudge below screen).
+		end
+
+		win:setFrame(
+			{
+				x = win:frame().x,
+				y = win:frame().y + 22,
+				w = win:frame().w,
+				h = win:frame().h,
+			}
+		);
+	end,
+};
 
 -- ==============================
 -- Windows
 -- ==============================
 
-hs.window.animationDuration = 0 -- Never animate things.
+hs.window.animationDuration = 0; -- Never animate things.
 
--- Center all newly created windows.
+hs.window.filter.new():subscribe( "windowCreated", functions.centerOnScreen ) -- Center all newly created windows.
+
+-- ==============================
+-- HazeOver Hacks
+-- ==============================
+
+-- When a window shows, poke HazeOver so it re-applies the overlay (sometimes it doesn't).
 hs.window.filter.new():subscribe(
-	"windowCreated",
-	function(win)
-		win:centerOnScreen(nil, false, 0)
+	{ "windowFocused", "windowVisible", "windowUnminimized", "windowCreated", "windowDestroyed" },
+	function()
+		-- Need to find a new poke method that works :(.
 	end
 )
 
-	-- ==============================
-	-- HazeOver Hacks
-	-- ==============================
-
-	-- When a window shows, poke HazeOver so it re-applies the overlay (sometimes it doesn't).
-	hs.window.filter.new():subscribe(
-		{ "windowFocused", "windowVisible", "windowUnminimized", "windowCreated", "windowDestroyed" },
-		function()
-			hs.osascript.applescript( [[ tell application "HazeOver" to set enabled to true ]] )
-		end
-	)
-
-	-- When there is no window for an app, disable the HazeOver overlay (beacuse it sticks around normally).
-	hs.window.filter.new():subscribe(
-		"hasNoWindows",
-		function()
-			hs.osascript.applescript( [[ tell application "HazeOver" to set enabled to false ]] )
-		end
-	)
+-- When there is no window for an app, disable the HazeOver overlay (beacuse it sticks around normally).
+hs.window.filter.new():subscribe(
+	"hasNoWindows",
+	function()
+		-- Need to find a new poke method that works :(.
+	end
+)
