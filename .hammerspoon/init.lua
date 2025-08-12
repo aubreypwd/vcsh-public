1ff17d972b31db277afbd4cd8c5d095d85e18033
Aubrey Portwood <code@aubreypwd.com>
2025-08-12 13:08:40 -0600

    Remove Hazeover hacks
    Nothing is really working.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,13 +2,18 @@
 -- Constants
 -- ==============================
 
-SECOND = 1000000;
+ONE_SECOND = 1000000;
 
 -- ==============================
 -- Functions
 -- ==============================
 
-functions = {
+fn = {
+
+	-- Easy sleep function.
+	sleep = function( seconds )
+		hs.timer.usleep( ONE_SECOND * seconds )
+	end,
 
 	-- ==============================
 	-- Windows
@@ -28,11 +33,15 @@ functions = {
 				return; -- The window should not be fucked with.
 			end
 
-			if functions.window.windowIsMaximized( win ) then
+			if fn.window.windowIsMaximized( win ) then
 				return; -- The window is already maximized, don't do center.
 			end
 
-			hs.timer.usleep( SECOND / 2 ); -- This makes it feel less jarring.
+			if true ~= win:isStandard() then
+				return; -- Only apply to standard windows.
+			end
+
+			fn.sleep( 1 / 4 );
 
 			-- Use macOS built in Window > Center via keyboard shortcuts (animated).
 			hs.eventtap.keyStroke( { 'ctrl', 'fn' }, 'c' );
@@ -41,11 +50,11 @@ functions = {
 
 		-- A way to discover if a window is already maximized.
 		windowIsMaximized = function( win )
-
-			return math.abs(win:frame().x - win:screen():frame().x) <= 2
-				and math.abs(win:frame().y - win:screen():frame().y) <= 2
-				and math.abs((win:frame().x + win:frame().w) - (win:screen():frame().x + win:screen():frame().w)) <= 2
-				and math.abs((win:frame().y + win:frame().h) - (win:screen():frame().y + win:screen():frame().h)) <= 2
+			return
+				math.abs(win:frame().x - win:screen():frame().x) <= 2
+					and math.abs(win:frame().y - win:screen():frame().y) <= 2
+					and math.abs((win:frame().x + win:frame().w) - (win:screen():frame().x + win:screen():frame().w)) <= 2
+					and math.abs((win:frame().y + win:frame().h) - (win:screen():frame().y + win:screen():frame().h)) <= 2
 		end,
 	},
 };
@@ -56,24 +65,4 @@ functions = {
 
 hs.window.animationDuration = 0; -- Never animate things.
 
-hs.window.filter.new():subscribe( "windowCreated", functions.window.centerOnScreen ) -- Center all newly created windows.
-
--- ==============================
--- HazeOver Hacks
--- ==============================
-
--- When a window shows, poke HazeOver so it re-applies the overlay (sometimes it doesn't).
-hs.window.filter.new():subscribe(
-	{ "windowFocused", "windowVisible", "windowUnminimized", "windowCreated", "windowDestroyed" },
-	function()
-		-- Need to find a new poke method that works :(.
-	end
-)
-
--- When there is no window for an app, disable the HazeOver overlay (beacuse it sticks around normally).
-hs.window.filter.new():subscribe(
-	"hasNoWindows",
-	function()
-		-- Need to find a new poke method that works :(.
-	end
-)
+hs.window.filter.new():subscribe( "windowCreated", fn.window.centerOnScreen ); -- Center all newly created windows.
