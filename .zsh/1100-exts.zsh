885c405a5dc1f194e93b70911a9e232691caa902
Aubrey Portwood <code@aubreypwd.com>
2025-08-28 16:14:39 -0600

    Make sure Sublime remains my editor.

--- .zsh/1100-exts.zsh
+++ .zsh/1100-exts.zsh
@@ -5,6 +5,7 @@
  #
  # @since Jul 14, 2023
  # @since Jun 12, 2024 Disabled because using OpenIn instead.
+ # @since August 28th 2025 Updated for sublime.
  ##
 reset-exts () {
 
@@ -14,4 +15,9 @@ reset-exts () {
 	# # Built-in Terminal
 	# duti -s com.apple.Terminal .sh all
 	# duti -s com.apple.Terminal .zsh all
+
+	# Make sure my editor is used for these files in Finder.
+	for ext in aspx bash c cfg class conf cpp cs css dart env go h hpp ini jar java js json log lua m markdown md mm php pl pm py pyw rb rs scss sh sql svelte swift toml ts tsx txt vb vue xml yaml yml zsh; do;
+		duti -s "$(osascript -e 'id of app "Sublime Text"')" "$ext" editor;
+	done
 }
