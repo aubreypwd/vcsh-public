616169e0bb0f0eaf6c37a6ab31c17b028594a80a
Aubrey Portwood <code@aubreypwd.com>
2025-08-29 17:29:12 -0600

    Always backup my db when terminal loads.
    I lost it today because of a brew upgrade, so this just makes a backup
    every once in a while.

--- .zsh/1000-mysql.zsh
+++ .zsh/1000-mysql.zsh
@@ -7,9 +7,12 @@
  # @since Nov 9, 2024 No longer using mysql
  ##
 
+# Always backup my mariadb db, just in case.
+mysqldump -u root --all-databases > "$HOME/.mariadb.bak.sql" 2>&1 &!
+
 reset-mysql () {
 
 	# Fix issue where PHP 8.1 can't access the WordPress DB.
-	#mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';"
+	# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';"
 }
 
